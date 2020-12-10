9dcb3a1af80ebfb47b66f5611a84fc604ea5d087
Dimitrios Tsatsoulas <b9013468@ncl.ac.uk>
2020-12-10 19:07:39 +0200

    create index request

--- cable_app/views.py
+++ cable_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    my_dic = {'insert_me': "Views.py"}
+    return render(request, 'cable_app/index.html', context=my_dic)
