dbba18c663788fea54fa705df580ccb39b651936
b9013867 <b.a.thompson-watson1@newcastle.ac.uk>
2020-12-15 11:10:21 +0000

    Database retrieval functionality added to cable app

--- cable_app/views.py
+++ cable_app/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
+from mainFDM.models import GameQuestion
+from random import choices
 from django.http import HttpResponse
 # Create your views here.
 
 
-arrayQnA = ''
-
-
 def index(request):
 
-    return render(request, 'cable_app/index.html',
-                  {'content1': arrayQnA})
+    question_list = GameQuestion.objects.filter()
+
+    questions_for_game = choices(question_list,k=4)
+
+    context = {
+        'question_1': questions_for_game[0].question,
+        'question_2': questions_for_game[1].question,
+        'question_3': questions_for_game[2].question,
+        'question_4': questions_for_game[3].question,
+        'answer_1': questions_for_game[0].answer,
+        'answer_2': questions_for_game[1].answer,
+        'answer_3': questions_for_game[2].answer,
+        'answer_4': questions_for_game[3].answer
+    }
+
+    return render(request, 'cable_app/index.html',context)
 
 
 
