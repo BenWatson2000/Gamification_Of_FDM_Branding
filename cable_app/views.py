b6303eee3241c0f545e21003beb2fdfadb1c1bc4
Dimitrios Tsatsoulas <b9013468@ncl.ac.uk>
2020-12-16 23:45:32 +0200

    added code that checks if questions are unique

--- cable_app/views.py
+++ cable_app/views.py
@@ -4,13 +4,37 @@ from random import choices
 from django.http import HttpResponse
 # Create your views here.
 
-#TODO get 4 random questions and answers with them being "UNIQUE"!
-
 def index(request):
 
     question_list = GameQuestion.objects.filter()
 
-    questions_for_game = choices(question_list,k=9)
+    mockArray = [None]*4
+    questions_for_game= [None]*4
+    counter =0
+
+    # making sure that every question is unique
+    while counter<4:
+        choice = choices(question_list, k=1)
+        if counter==0:
+            mockArray[0]=choice[0].question
+            questions_for_game[0] = choice[0]
+            counter+=1
+        elif counter==1:
+            if mockArray[0]!= choice[0].question:
+                mockArray[1] = choice[0].question
+                questions_for_game[1] = choice[0]
+                counter += 1
+        elif counter==2:
+            if mockArray[0]!= choice[0].question and mockArray[1]!= choice[0].question:
+                mockArray[2] = choice[0].question
+                questions_for_game[2] = choice[0]
+                counter += 1
+        elif counter==3:
+            if mockArray[0]!= choice[0].question and mockArray[1]!= choice[0].question and mockArray[2]!= choice[0].question:
+                mockArray[3] = choice[0].question
+                questions_for_game[3] = choice[0]
+                counter += 1
+
 
     context = {
         'question_1': questions_for_game[0].question,
