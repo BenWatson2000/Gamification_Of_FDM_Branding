008c81e7ccab71eeca14ac4fe6fe5ac66e62b275
lakshman <l.santhanam1@newcastle.ac.uk>
2020-12-17 20:43:08 +0000

    timer functionality added to pipe and memory games

--- memoryApp/static/js/app.js
+++ memoryApp/static/js/app.js
@@ -123,6 +123,33 @@ document.addEventListener('DOMContentLoaded', ()=>{
         }
     }
 
+    function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = 0;
+            if(alert('time has run out')){}
+            else window.location.reload();
+            // timer = duration; // uncomment this line to reset timer automatically after reaching 0
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var time = 60 / 2, // your time in seconds here
+        display = document.querySelector('#timer');
+    startTimer(time, display);
+};
+
+
     //flip panel function
     function flipPanel(){
         var panelId = this.getAttribute('data-id')
