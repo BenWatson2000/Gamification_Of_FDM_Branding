bcf18ca87965949f79aa7b2b8e13dab89b4aa4ea
root <root@ip-172-31-23-236.eu-west-2.compute.internal>
2021-01-11 17:17:19 +0000

    alteration to settings.py file to make server hosting work

--- myApp/settings.py
+++ myApp/settings.py
@@ -125,10 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_URL = '/helper/login/'
 LOGIN_REDIRECT_URL = '/helper/home/'
