d9ab3408ae7e961ac6176946681db7e9b1872fb4
b9013867 <b.a.thompson-watson1@newcastle.ac.uk>
2021-01-15 15:30:18 +0000

    fixed twitter url

--- mainFDM/views.py
+++ mainFDM/views.py
@@ -226,8 +226,8 @@ def results(request):
                 'stream_type': stream_type,
                 'game': game_played,
                 'tweetURL': 'https://twitter.com/intent/tweet?'
-                            'text=I%20just%20scored%20' + score_got + '%20on%20the%20' + game_played + ' Game%21%20Try'
-                            '%20and%20beat%20my%20score%20at%20https%3A//mycareerpath.co.uk%20and%20di'
+                            'text=I%20just%20got%20a%20time%20of%20'+score_got+'%20on%20the%20'+game_played+' Game%21%20Try'
+                            '%20and%20beat%20my%20time%20at%20https%3A//mycareerpath.co.uk%20and%20di'
                             'scover%20many%20different%20career%20sectors%20in%20technology%21&hashtags=MYCAREERPATH',
             }
             return render(request, 'mainFDM/results.html', context)
