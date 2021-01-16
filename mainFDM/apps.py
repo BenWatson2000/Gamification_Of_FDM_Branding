04db33370f623c67217276c8a903ed45dc195ed1
Kirai <i.koryciarz@ncl.ac.uk>
2021-01-16 22:38:59 +0000

    Feature branch completed. Added admin approval functionality to the helper login by extending the django User model with a custom one-to-one field. Now every new user will have an attribute 'admin_approved' in the HelperProfile table set to False and won't be able to log in before an admin changes this to True via the django admin page.

--- mainFDM/apps.py
+++ mainFDM/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class MainConfig(AppConfig):
     name = 'mainFDM'
+
+    # make sure to load the signals file
+    def ready(self):
+        from mainFDM import signals
